4dcce402aba6f96e59fa92c90b2b0aa4a05b9a11
Fernando F D Bittencourt <fernandodbittencourt@hotmail.com>
2026-04-07 14:53:48 -0300

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost.length<2){
+            return 0;
+        }
+        int prev1=cost[0];
+        int prev2=cost[1];
+
+        for(int i=2;i<cost.length;i++){
+            int curr = cost[i]+Math.min(prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
+        }
+        return Math.min(prev1,prev2);
+    }
+}
