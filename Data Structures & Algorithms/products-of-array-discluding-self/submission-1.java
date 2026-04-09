da2238a4b0c4da982de0c297557ab4c3c01f3c03
Fernando F D Bittencourt <fernandodbittencourt@hotmail.com>
2026-04-09 13:36:05 -0300

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        
+        // Produto à esquerda
+        output[0] = 1;
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+        
+        // Produto à direita e multiplicando
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+        
+        return output;
+    }
+}  
