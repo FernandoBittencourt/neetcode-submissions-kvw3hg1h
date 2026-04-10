0814dc10d2be965e53e0a9c748288503b7810e49
Fernando F D Bittencourt <fernandodbittencourt@hotmail.com>
2026-04-10 15:41:32 -0300

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        String hashS1=hash(s1);
+        for(int i=0;i<s2.length();i++){
+            StringBuilder substringBuilder=new StringBuilder();
+            for(int j=0;j<s1.length();j++){
+                if(j+i>=s2.length()){
+                    return false;
+                } else{
+                    substringBuilder.append(s2.charAt(j+i));
+                }
+            }
+             String hashS2= hash(substringBuilder.toString());
+             if(hashS2.equals(hashS1)){
+                return true;
+             }
+        }
+        return false;
+    }
+
+    private String hash(String s){
+        int[] countLetter=new int[26];
+        for(int i=0;i<s.length();i++){
+            countLetter[s.charAt(i)-'a']++;
+        }
+        StringBuilder sBuilder=new StringBuilder();
+        for(int i=0;i<countLetter.length;i++){
+            sBuilder.append(countLetter[i]);
+            sBuilder.append("#");
+        }
+        return sBuilder.toString();
+    }
+}
