241a3022ee04447d07dabe1764f7a49642447818
Fernando F D Bittencourt <fernandodbittencourt@hotmail.com>
2026-04-10 15:13:47 -0300

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int res = 0;
+
+        for (char target = 'A'; target <= 'Z'; target++) {
+            int left = 0;
+            int trocas = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                // se for diferente da letra alvo, precisa trocar
+                if (s.charAt(right) != target) {
+                    trocas++;
+                }
+
+                while (trocas > k) {
+                    if (s.charAt(left) != target) {
+                        trocas--;
+                    }
+                    left++;
+                }
+
+                // atualiza melhor tamanho
+                res = Math.max(res, right - left + 1);
+            }
+        }
+
+        return res;
+    }
+}
